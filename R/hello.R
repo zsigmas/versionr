3bde401c7d0729467ac93dd0ad6c4e35ccb02caf
Luis Morís Fernández <luis.moris.fernandez+gitkraken@gmail.com>
2020-10-14 10:53:29 +0200

    Prepare Basic Functions

---
+++ R/hello.R
@@ -0,0 +1,26 @@
+update_version <- function() {
+  con <- file('DESCRIPTION')
+  on.exit(close(con))
+
+  desc_content <- readLines(con)
+  version_idx  <- which(startsWith(desc_content, 'Version:'))
+  date_idx  <- which(startsWith(desc_content, 'Date:'))
+  # browser()
+  desc_content[version_idx] <- paste('Version:', clean_version_number(get_describe_head()))
+  desc_content[date_idx] <- paste('Date:', date())
+  desc_content
+
+}
+
+get_describe_head <- function() {
+  if(!dir.exists('.git')){stop("Can't find .git directory in specified path")}
+  system2(command="git", args="describe HEAD --tags | rev | sed 's/g-/./' | sed 's/-/+/' | rev", stdout=T)
+}
+
+clean_version_number <- function(ver_num) {
+  spl_ver_num <- unlist(strsplit(ver_num, split='+', fixed = T))
+  ver_num <- spl_ver_num[1]
+  commit_num <- unlist(strsplit(spl_ver_num[2], split='.', fixed = T))[1]
+  commit_num <- ifelse(!is.na(commit_num),paste0('-', commit_num), '')
+  return(paste0(ver_num, commit_num))
+}
