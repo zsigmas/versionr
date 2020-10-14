98fc82a62bf095d3499917716d5a5a474e8b4abb
Luis Morís Fernández <luis.moris.fernandez+gitkraken@gmail.com>
2020-10-14 11:46:46 +0200

    Add current_version function

--- R/hello.R
+++ R/hello.R
@@ -11,6 +11,16 @@ update_version <- function(path='.') {
   writeLines(desc_content, con = con)
 }
 
+current_version <- function(path='.') {
+  con <- file('DESCRIPTION')#
+  on.exit(close(con))
+
+  desc_content <- readLines(con)
+  version_idx  <- which(startsWith(desc_content, 'Version:'))
+  current_version <- unlist(strsplit(desc_content[version_idx], split=': ', fixed = T))[2]
+  return(current_version)
+}
+
 get_describe_head <- function() {
   if(!dir.exists('.git')){stop("Can't find .git directory in specified path")}
   system2(command="git", args="describe HEAD --tags | rev | sed 's/g-/./' | sed 's/-/+/' | rev", stdout=T)
